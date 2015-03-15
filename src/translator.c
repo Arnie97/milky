452cb5fe5d630d2ea55844a7001018ce273eb928
Arnie97 <arnie97@gmail.com>
2015-03-15 20:46:47 +0800

    Looks one token ahead to perform recursion.

--- src/translator.c
+++ src/translator.c
@@ -5,6 +5,30 @@
 #include "indent.h"
 #include "translator.h"
 
+#define NONEXISTENT (0xFF)
+
+static Token look_ahead_token;
+
+void
+next_token(Token *token)
+{
+    if (look_ahead_token->type == NONEXISTENT) {
+        dprintf(("fwd >"));
+        get_indent(token);
+    } else {
+        dprintf(("pop >"));
+        *token = look_ahead_token;
+        look_ahead_token->type = NONEXISTENT;
+    }
+}
+
+void
+store_token(Token *token)
+{
+    dprintf(("push> "));
+    look_ahead_token = *token;
+}
+
 void
 parse_file(void)
 {
@@ -13,7 +37,7 @@ parse_file(void)
     look_ahead_queue(INIT);
     do {
         int prev_pos = line_pos;
-        get_indent(&token);
+        next_token(&token);
         dprintf((token.kind > 0x20?
             "look_ahead..%d(%d,%d)->%d  kind..%c  str..[%s]":
             "look_ahead..%d(%d,%d)->%d  kind..%d  str..[%s]",
