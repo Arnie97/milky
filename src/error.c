d5030a10a52d956e752aeabfb55919412672f82b
Arnie97 <arnie97@gmail.com>
2015-03-23 01:29:09 +0800

    Create a user-friendly error display.

--- src/error.c
+++ src/error.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "input.h"
 #include "token.h"
@@ -9,10 +10,28 @@ void
 throw(int error_code, char *message, Token *token)
 {
     if (token != NULL) {
-        fprintf(stderr, "\n%s:%d:%d: %s\n",
+        int scanned = 0;
+        char *cursor = line;
+        fprintf(stderr, "\n\n\033[36;1m%s:\033[32;1m%d:%d: \033[31;1m%s\033[0m\n",
             file_name, token->row, token->column, message);
+        while (scanned < token->row - 1) {
+            if (*++cursor == '\n') {
+                scanned++;
+            }
+        }
+        scanned = 0;
+        while (scanned++ < token->column) {
+            putc(*cursor++, stderr);
+        }
+        fprintf(stderr, "\033[33;1m%s\033[0m", token->str);
+        cursor += strlen(token->str);
+        while (*cursor != '\n') {
+            putc(*cursor++, stderr);
+        }
+        fprintf(stderr, "\033[0m\n%*s\n\n", token->column, "^");
     } else {
-        fprintf(stderr, "\n%s: %s\n", file_name, message);
+        fprintf(stderr, "\n\n\033[36;1m%s: \033[31;1m%s\033[0m\n\n",
+            file_name, message);
     }
     exit(error_code);
 }
