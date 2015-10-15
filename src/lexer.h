032b48b9f76cb77bbf7af8591f3259ed879393c8
Arnie97 <arnie97@gmail.com>
2015-10-15 21:08:35 +0800

    Lexer Milkification.

--- src/lexer.h
+++
@@ -1,9 +0,0 @@
-#ifndef LEXER_H_INCLUDED
-#define LEXER_H_INCLUDED
-
-#define MAX_INDENTATION_LEVEL (20)
-
-int get_token(Token *token);
-void get_whitespace(Token *token, int pos_in_token);
-
-#endif /* LEXER_H_INCLUDED */
